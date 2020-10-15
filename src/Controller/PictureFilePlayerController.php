3c94e5c5ce2a53cffcc792d92bcac23f5fb97eaf
Th-Fauconnier <th.fauconnier@outlook.fr>
2020-10-15 16:33:36 +0200

    image trully ok,

--- src/Controller/PictureFilePlayerController.php
+++ src/Controller/PictureFilePlayerController.php
@@ -11,9 +11,12 @@ class PictureFilePlayerController
 {
     public function __invoke(Player $data)
     {
-        $dir = __DIR__.'\..\..\public\storage\images\\';
+        $path = 'storage\images\\'.$data->getPicture();
+        $type = pathinfo($path, PATHINFO_EXTENSION);
+        $data = file_get_contents($path);
+        $pictureBase64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
 
-        $image = imagecreatefromjpeg($dir.$data->getPicture());
-        dd($image);
+        //$image = imagecreatefromjpeg('storage\images\\'.$data->getPicture());
+        return $pictureBase64;
     }
 }
