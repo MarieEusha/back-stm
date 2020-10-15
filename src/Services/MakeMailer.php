fa774dfd0e89318a16ca0c88190cbba601aa88ad
Phanu <phanuelgadenne@yahoo.fr>
2020-10-15 23:16:37 +0200

    template classicMail + function static sendMultiMail + MailerController

--- src/Services/MakeMailer.php
+++ src/Services/MakeMailer.php
@@ -34,4 +34,46 @@ class MakeMailer extends AbstractController
 
         $mailer->send($email);
     }
+
+    /**
+     * @param String $from
+     * @param array $to
+     * @param String $subject
+     * @param String $template
+     * @param array $context
+     * @param MailerInterface $mailer
+     * @throws TransportExceptionInterface
+     */
+    static public function sendMultiMails(String $from, Array $to, String $subject, String $template, Array $context, MailerInterface $mailer)
+    {
+        $email = (new TemplatedEmail())
+                ->from($from)
+                ->to($to[0])
+                ->subject($subject)
+                ->htmlTemplate($template)
+                ->context($context);
+        for($i = 1; $i < count($to); $i++){
+            $email->addTo($to[$i]);
+        }
+
+        $mailer->send($email);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
